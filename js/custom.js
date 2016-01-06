998401946ec6c95e02ef7f572f5797a0a00c464b
Mads Buch <madspbuch@gmail.com>
2016-01-06 15:02:23 +0100

    updating to a newer version of template

--- js/custom.js
+++ js/custom.js
@@ -1,32 +1,76 @@
-/* ---------------------------------------------------------------------- */
-/*	Analytics
-/* ---------------------------------------------------------------------- */
-window.analytics=window.analytics||[],window.analytics.methods=["identify","group","track","page","pageview","alias","ready","on","once","off","trackLink","trackForm","trackClick","trackSubmit"],window.analytics.factory=function(t){return function(){var a=Array.prototype.slice.call(arguments);return a.unshift(t),window.analytics.push(a),window.analytics}};for(var i=0;i<window.analytics.methods.length;i++){var key=window.analytics.methods[i];window.analytics[key]=window.analytics.factory(key)}window.analytics.load=function(t){if(!document.getElementById("analytics-js")){var a=document.createElement("script");a.type="text/javascript",a.id="analytics-js",a.async=!0,a.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.io/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n)}},window.analytics.SNIPPET_VERSION="2.0.9",
-window.analytics.load("uyc1ueeeys");
-window.analytics.page();
-
-jQuery(document).ready(function(){
-	
-	
+jQuery(document).ready(function(){ 
 	
 	/* ---------------------------------------------------------------------- */
 	/*	Custom Functions
 	/* ---------------------------------------------------------------------- */
-	
-	// Needed variables
+
+	// Logo
 	var $logo 	= $('#logo');
-		
+
+    if (location.href.indexOf("#") != -1) {
+        $logo.show();
+    }
 	// Show logo 
-	$('.tab-resume,.tab-portfolio,.tab-contact').click(function(e) {
-	  window.analytics.page(e.currentTarget.innerHTML);
+	$('.menu .tabs a').click(function() {
 	  $logo.fadeIn('slow');
 	});
 	// Hide logo
-	$('.tab-profile').click(function(e) {
-	  window.analytics.page(e.currentTarget.innerHTML);
+	$('.tab-profile').click(function() {
 	  $logo.fadeOut('slow');
 	});	
+
+	/* ---------------------------------------------------------------------- */
+	/*	Resume
+	/* ---------------------------------------------------------------------- */
 	
+	// Rating bars
+	$(".skills li .rating").each(function(index,e) { 
+
+		// Vars
+		var 
+			$ratNum = 7,
+			$rat = $(e).attr("data-rat"),
+			$point = "<span></span>";
+
+		// Append points
+		while($ratNum > 0){
+		     $(e).append($point);
+		     $ratNum--;
+		}
+
+		$(e).find("span").each(function(index,e) { 
+			if(index >= $rat) return false;
+			// Append Disabled Rats
+			$(e).animate({
+			    opacity: 1
+			  });
+		});
+
+    });
+
+	/* ---------------------------------------------------------------------- */
+	/*	About
+	/* ---------------------------------------------------------------------- */
+	
+	// Profile Photo Slider
+	 $(".photo-inner ul").carouFredSel({
+        direction           : "left",
+ 	    circular: true,
+        auto    			: true,
+        scroll 			: {
+            items           : 1,
+            fx 				: 'crossfade',
+            duration        : 1500,                        
+            wipe    		: true
+        },
+	    swipe: {
+	        onTouch: true
+	    },
+        items: {
+            width: 153
+        }           
+    });
+	 
 	/* ---------------------------------------------------------------------- */
 	/*	Menu
 	/* ---------------------------------------------------------------------- */
@@ -44,6 +88,7 @@ jQuery(document).ready(function(){
 	  tabs				:".tmenu",
 	  tabActiveClass	:'active',
 	});
+
 	
 	// Hover menu effect
 	$content.find('.tabs li a').hover(
@@ -53,16 +98,62 @@ jQuery(document).ready(function(){
 			$(this).stop().animate({ marginTop: "0px" }, 300);
 		}
 	);
+
+	// Menu Navigation
+	 $(".menu .tabs").carouFredSel({
+        responsive          : true,
+        direction           : "left",
+ 	    circular: false,
+    	infinite: false,
+        pagination  		: "#menu-controls",  
+        auto    			: false,
+        scroll 			: {
+            items           : 1,
+            duration        : 300,                        
+            wipe    : true
+        },
+		prev	: {	
+			button	: "#menu-prev",
+			key		: "right"
+		},
+		next	: { 
+			button	: "#menu-next",
+			key		: "left"
+		},
+	    swipe: {
+	        onTouch: true
+	    },
+        items: {
+            width: 140,
+            visible: {
+              min: 2,
+              max: 5
+            }
+        }           
+    });
+	/* ---------------------------------------------------------------------- */
+	/*	Cats Filter
+	/* ---------------------------------------------------------------------- */ 
+	
+	var $catsfilter 		= $('.cats-filter');
+
+	// Copy categories to item classes
+	$catsfilter.find('a').click(function() {
+		var currentOption = $(this).attr('data-filter');
+		$(this).parent().parent().find('a').removeClass('current');
+		$(this).addClass('current');
+	});	
+
 	/* ---------------------------------------------------------------------- */
 	/*	Portfolio
 	/* ---------------------------------------------------------------------- */ 
 	
 	// Needed variables
-	var $container	 	= $('#portfolio-list');
-	var $filter 		= $('#portfolio-filter');
+	var $plist	 	= $('#portfolio-list');
+	var $pfilter 		= $('#portfolio-filter');
 		
 	// Run Isotope  
-	$container.isotope({
+	$plist.isotope({
 		filter				: '*',
 		layoutMode   		: 'masonry',
 		animationOptions	: {
@@ -72,9 +163,9 @@ jQuery(document).ready(function(){
 	});	
 	
 	// Isotope Filter 
-	$filter.find('a').click(function(){
+	$pfilter.find('a').click(function(){
 	  var selector = $(this).attr('data-filter');
-		$container.isotope({ 
+		$plist.isotope({ 
 		filter				: selector,
 		animationOptions	: {
 		duration			: 750,
@@ -86,31 +177,53 @@ jQuery(document).ready(function(){
 	});	
 	
 	// Portfolio image animation 
-	$container.find('img').adipoli({
+	$plist.find('img').adipoli({
 		'startEffect' 	: 'transparent',
 		'hoverEffect' 	: 'boxRandom',
 		'imageOpacity' 	: 0.6,
 		'animSpeed' 	: 100,
 	});
 	
-	// Copy categories to item classes
-	$filter.find('a').click(function() {
-		var currentOption = $(this).attr('data-filter');
-		$filter.find('a').removeClass('current');
-		$(this).addClass('current');
-	});	
-	
 	/* ---------------------------------------------------------------------- */
-	/*	Fancybox 
+	/*	prettyPhoto
 	/* ---------------------------------------------------------------------- */
-	$container.find('.folio').fancybox({
-		'transitionIn'		:	'elastic',
-		'transitionOut'		:	'elastic',
-		'speedIn'			:	200, 
-		'speedOut'			:	200, 
-		'overlayOpacity'	:   0.6
-	});
 
+    $("a[rel^='portfolio']").prettyPhoto({
+    	animation_speed: 'fast', /* fast/slow/normal */
+    	social_tools: '',
+    	theme: 'pp_default',
+    	horizontal_padding: 5,
+    });
+
+	/* ---------------------------------------------------------------------- */
+	/*	Contact Form
+	/* ---------------------------------------------------------------------- */
+	
+	// Needed variables
+	var $contactform 	= $('#contactform'),
+		$success		= 'Your message has been sent. Thank you!';
+		
+	$contactform.submit(function(){
+		$.ajax({
+		   type: "POST",
+		   url: "php/contact.php",
+		   data: $(this).serialize(),
+		   success: function(msg)
+		   {
+				if(msg == 'SEND'){
+					response = '<div class="success">'+ $success +'</div>';
+				}
+				else{
+					response = '<div class="error">'+ msg +'</div>';
+				}
+				// Hide any previous response text
+				$(".error,.success").remove();
+				// Show response message
+				$contactform.prepend(response);
+			}
+		 });
+		return false;
+	});	
 	/* ---------------------------------------------------------------------- */
 	/*	Google Maps
 	/* ---------------------------------------------------------------------- */
@@ -118,19 +231,22 @@ jQuery(document).ready(function(){
 	// Needed variables
 	var $map 				= $('#map'),
 		$tabContactClass 	= ('tab-contact'),
-		$address 			= 'Møllevangs Allé 142 8200 Aarhus N';
-	
+		$lat 				= '-37.81759',
+		$lon 				= '144.964557';
+
+	$map.gmap().bind('init', function(ev, map) {
+		$map.gmap('addMarker', {'position': $lat +','+ $lon  , 'bounds': true}).click(function() {
+			$map.gmap('openInfoWindow', {'content': 'Hello World!'}, this);
+		});
+		$map.gmap('option', 'zoom', 16);
+	});
+
+	// Refresh Map
 	$content.bind('easytabs:after', function(evt,tab,panel) {
-		if ( tab.hasClass($tabContactClass) ) {
-			$map.gMap({
-				address: $address,
-				zoom: 16,
-				markers: [
-					{ 'address' : $address }
-				]
-			});
-		}
+		$map.gmap('refresh'); 
   	});
-	
+
+      
+  
 
 });	
